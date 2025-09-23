f27bde41a186fc99b555aa28222ed3679e9c8690
Jose Gomez <josepilar3@gmail.com>
2025-09-22 23:51:51 -0300

    Agregar clase 1,2 y 3 de Python

---
+++ Python/Clase 1, 2 y 3/tipo_str.py
@@ -0,0 +1,12 @@
+# Profundizando en el tipo String
+# Concatenación automática en Python
+
+variable = 'Adiós'
+mensaje = 'Hola' + ' ' + 'gente! ' + variable # 'Hola ''Alumnos'
+mensaje += ', Terminamos..'
+# print(mensaje)
+
+# Usamos la clase help para Ayuda o documentación en python (built-in)
+# help(str.capitalize)
+import math
+help(math.isnan)
