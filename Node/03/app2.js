d601d19027525a8755eae89bc75fa92790a92bae
Vitors <mendoza24@gmail.com>
2025-11-25 01:30:53 -0300

    Agregando el modulo 03 para el desarrollo de Node

---
+++ Node/03/app2.js
@@ -0,0 +1,10 @@
+// Creamos una funcion flecha
+/**
+ * Función para generar un saludo personalizado
+ * @param {string} nombre - El nombre a saludar
+ */
+const saludar = (nombre) => {
+  return `Saludos ${nombre}`;
+};
+
+console.log(saludar("UFO Development"));
